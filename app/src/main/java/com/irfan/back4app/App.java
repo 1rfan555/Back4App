3b5f4c524e9d7c52d25e11f54dcc79ab78b27103
1rfan555 <irfan.inamdar1234@gmail.com>
2018-10-25 12:39:34 +0530

    bg

--- app/src/main/java/com/irfan/back4app/App.java
+++ app/src/main/java/com/irfan/back4app/App.java
@@ -6,12 +6,9 @@ import com.parse.ParseInstallation;
 
 public class App extends Application {
 
-
-
     @Override
     public void onCreate() {
         super.onCreate();
-
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
                 .clientKey(getString(R.string.back4app_client_key))
